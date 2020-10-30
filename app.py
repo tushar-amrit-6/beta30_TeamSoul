b34a2fdd9d364a416f5a23875a4287bcbad0cc58
Seshal Jain <seshaljain11@gmail.com>
2020-10-31 04:38:37 +0530

    Add static patient dashboard

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/patient/dashboard')
+def patient_dashboard():
+    return render_template('patient-dashboard.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
